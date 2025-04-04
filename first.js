3a9c597919833f0f080f57f1bf4fd1cfafdaa182
sahanar <sahanaramesh2022@gmail.com>
2025-04-04 16:59:08 +0530

    new file

---
+++ first.js
@@ -0,0 +1,5 @@
+function addNum(a, b) {
+    return a + b;
+}
+var sum = addNum(12, 8);
+console.log('Sum:' + sum);
---
+++ first.ts
@@ -0,0 +1,7 @@
+function addNum(a:number, b:number)
+{
+    return a + b
+}
+
+let sum = addNum(12,8);
+console.log('Sum:'+ sum);
